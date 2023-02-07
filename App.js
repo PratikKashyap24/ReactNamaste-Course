721c19120230b238a26d2a8dbe82b9172bcbd44c
Pratik <pratikkashyap98@gmail.com>
2023-02-08 01:53:39 +0530

    Chapter3

--- App.js
+++ App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import { React } from "react";
 import ReactDOM from "react-dom/client";
-const heading1 = React.createElement(
+const heading1 = createElement(
   "h1",
   {
     id: "title",
+    key: "h1",
   },
   "Heading 1"
 );
-const heading2 = React.createElement(
+const heading2 = createElement(
   "h2",
   {
     id: "title",
+    key: "h2",
   },
   "Heading 2"
 );
